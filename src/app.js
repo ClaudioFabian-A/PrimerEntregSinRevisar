07f9ac0a9127bb6c5529cbe7fb3e81a00b92c657
claudio <gonzalez.claudiofabian3@gmail.com>
2023-08-19 23:39:34 -0300

    10

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 import express from "express"
 import productRouter from "./routes/products.router.js"
 import cartRouter from "./routes/carts.router.js"
+import viewsRouter from "./routes/views.router.js"
+import productManager from "./managers/productManager.js"
+import { __dirname } from "./utils.js"
+import { Server } from "socket.io";
+import handlebars from "express-handlebars"
+
+
 const app = express();
 const PORT = 8080;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.static(`${__dirname}/public`));
+app.engine('handlebars', handlebars.engine())
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'handlebars')
 
 
 app.use("/api/Products", productRouter)
 app.use("/api/Carts", cartRouter)
-app.use("/", viewRouter)
-
-
-app.listen(PORT, () => {
+app.use("/", viewsRouter)
+const httpSocketServer = app.listen(PORT, () => {
     console.log(`Server on PORT : ${PORT}`);
+})
+
+const manager = new productManager(__dirname + "/files/Products.json")
+const serverSocket = new Server(httpSocketServer);
+
+serverSocket.on("connect", async (socket)=> {
+    const products1 = await manager.getProducts({});
+    serverSocket.emit("lista", products1);
 })
